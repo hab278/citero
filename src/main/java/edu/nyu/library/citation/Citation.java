1c0adbb49fa860b028fbb94e51515575ecc0c381
hab278 <hab278@nyu.edu>
2012-07-05 14:30:19 -0400

    Added descriptive documentation

--- src/main/java/edu/nyu/library/citation/Citation.java
+++ src/main/java/edu/nyu/library/citation/Citation.java
@@ -1,18 +1,18 @@
 package edu.nyu.library.citation;
 
-/** Description of Citation
+/** The citation class is the tool required to start
+ * the data interchange process. 
  * 
  * @author hab278
  * @version 1.0.0-alpha.1
  */
 public class Citation {
 
-	/** Description of Citation()
+	/** The default constructor instantiates the citation
+	 * with no data payload and no formats available.
 	 * 
 	 */
 	public Citation() 
 	{
 	}
-	
-	
 }
