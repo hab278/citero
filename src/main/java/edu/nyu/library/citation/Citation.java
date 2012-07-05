084e7b7be02998666341b464315ea5c982c7bf97
hab278 <hab278@nyu.edu>
2012-07-05 13:28:54 -0400

    Initial framework
    Initial tests defined, initial skeleton of Citation

---
+++ src/main/java/edu/nyu/library/citation/Citation.java
@@ -0,0 +1,18 @@
+package edu.nyu.library.citation;
+
+/** Description of Citation
+ * 
+ * @author hab278
+ * @version 1.0.0-alpha.1
+ */
+public class Citation {
+
+	/** Description of Citation()
+	 * 
+	 */
+	public Citation() 
+	{
+	}
+	
+	
+}
---
+++ src/test/java/edu/nyu/library/citation/CitationTest.java
@@ -0,0 +1,24 @@
+package edu.nyu.library.citation;
+
+//import static org.junit.Assert.assertTrue;
+//import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CitationTest {
+
+		
+	@Test
+	public void testRecognizedFormat() {
+	}
+	
+	@Test
+	public void testUnmatchedFormat(){
+		
+	}
+	
+	@Test
+	public void testSizeLimit(){
+		
+	}
+}
