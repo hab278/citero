03930d173e5572249e8be586d686043f515efb43
hab278 <hab278@nyu.edu>
2012-07-06 12:29:38 -0400

    Updated skeleton with comments for API.

--- src/main/java/edu/nyu/library/citation/Citation.java
+++ src/main/java/edu/nyu/library/citation/Citation.java
@@ -1,5 +1,6 @@
 package edu.nyu.library.citation;
 
+
 /** The citation class is the tool required to start
  * the data interchange process. 
  * 
@@ -8,13 +9,42 @@ package edu.nyu.library.citation;
  */
 public class Citation {
 
+	private String data;
+	
 	/** The default constructor instantiates the citation
 	 * with no data payload and no formats available.
-	 * 
+	 * It creates a Citation instance.
 	 */
-	public Citation() 
-	{
+	public Citation(){
 	}
+	
+	/** Creates a Citation instance and loads the provided
+	 * data.
+	 * 
+	 * @param data Input data represented as a string
+	 * @param inputFormat Input format specified via string
+	 */
+	public Citation(String data, String inputFormat){
+		loadData(data,inputFormat);
+	}
+	
+	/** Loads data into Citation after converting it to
+	 * a common format.
+	 * 
+	 * @param data
+	 * @param inputFormat
+	 */
+	public void loadData(String data, String inputFormat){
+	}
+	
+	/**
+	 * 
+	 * @param outputFormat The format the data should be converted to 
+	 * @return returns data formatted to the specified output format in string representation.
+	 */
+	public String output(String outputFormat){
+		return "";
+	}
+	
+	
 }
-
-//jenkins test2
