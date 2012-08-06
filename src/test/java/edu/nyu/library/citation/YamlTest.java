fc65cda0e02f89dd402ab7178b740e988d4618af
hab278 <as>
2012-08-06 12:40:14 -0400

    load Many Dcuments

--- src/test/java/edu/nyu/library/citation/YamlTest.java
+++ src/test/java/edu/nyu/library/citation/YamlTest.java
@@ -10,18 +10,29 @@ import org.yaml.snakeyaml.Yaml;
 public class YamlTest {
 
 	@Test
-	public void loadYaml(){
+	public void testLoadYaml(){
 		Yaml yaml = new Yaml();
 		Object obj = yaml.load("a: 1\nb: 2\nc:\n  - aaa\n  - bbb");
 		assertEquals(obj.toString(), "{a=1, b=2, c=[aaa, bbb]}");
 	}
 	
 	@Test
-	public void dumpYaml(){
+	public void testDumpYaml(){
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("YAML", "test");
 		Yaml yaml = new Yaml();
 		String output = yaml.dump(map);
 		assertEquals(output, "{YAML: test}\n");
+	}
+	
+	@Test
+	public void testLoadManyDoc(){
+		String input = "Note:\n---Note:\n---Note:\n---Note:\n---Note:\n---";
+		Yaml yaml = new Yaml();
+		int counter = 0;
+		for(Object data : yaml.loadAll(input)){
+			counter++;
+		}
+		assertEquals(5,counter);
 	}
 }
