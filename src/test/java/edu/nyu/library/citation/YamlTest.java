c43c0b19f4db00d0492afa1bd59526aaad52951a
hab278 <as>
2012-08-06 11:38:45 -0400

    Yaml dumping test

--- src/test/java/edu/nyu/library/citation/YamlTest.java
+++ src/test/java/edu/nyu/library/citation/YamlTest.java
@@ -1,6 +1,9 @@
 package edu.nyu.library.citation;
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.*;
 import org.yaml.snakeyaml.*;
 
@@ -11,5 +14,14 @@ public class YamlTest {
 		Yaml yaml = new Yaml();
 		Object obj = yaml.load("a: 1\nb: 2\nc:\n  - aaa\n  - bbb");
 		assertEquals(obj.toString(), "{a=1, b=2, c=[aaa, bbb]}");
+	}
+	
+	@Test
+	public void dumpYaml(){
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("YAML", "test");
+		Yaml yaml = new Yaml();
+		String output = yaml.dump(map);
+		assertEquals(output, "---\nYAML: test");
 	}
 }
