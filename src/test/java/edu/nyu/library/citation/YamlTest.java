3f47f5c242f163cddf904928e9b7ec1ff3a51f3b
hab278 <as>
2012-08-14 17:11:51 -0400

    removed debugging printouts

--- src/test/java/edu/nyu/library/citation/YamlTest.java
+++ src/test/java/edu/nyu/library/citation/YamlTest.java
@@ -69,7 +69,6 @@ public class YamlTest {
 	    constructor.addTypeDescription(itemDescription);
 	    Yaml yaml1 = new Yaml(constructor);
 	    CSF i = (CSF)yaml1.load("itemType: patent\ncreator: {inventor: null}\nfields: {title: null}");
-	    System.out.println(i.toString());
 	    assertEquals("itemType: patent\ncreator: {inventor: null}\nfields: {title: null}", i.toString());
 	}
 	
