80a7a26b31ba67a14223520c136499150e56f35a
hab278 <as>
2012-08-15 16:17:58 -0400

    Temporary test cases

--- src/test/java/edu/nyu/library/citation/FormatsTest.java
+++ src/test/java/edu/nyu/library/citation/FormatsTest.java
@@ -2,11 +2,11 @@ package edu.nyu.library.citation;
 
 public class FormatsTest {
 
-	public final static  String RIS = "",
-			PNX = "",
-			OPENURL = "",
-			XERXES_XML = "",
-			BIBTEX = "",
-			CSF = "";
+	public final static  String RIS = "test",
+			PNX = "test",
+			OPENURL = "test",
+			XERXES_XML = "test",
+			BIBTEX = "test",
+			CSF = "test";
 	
 }
