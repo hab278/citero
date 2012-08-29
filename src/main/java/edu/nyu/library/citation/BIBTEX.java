dfa97a052e5516f6d80dce26ed742cd8d16e0c3c
hab278 <as>
2012-08-29 16:05:07 -0400

    BIBTEX import

--- src/main/java/edu/nyu/library/citation/BIBTEX.java
+++ src/main/java/edu/nyu/library/citation/BIBTEX.java
@@ -2,21 +2,40 @@ package edu.nyu.library.citation;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.configuration.ConfigurationException;
 
 import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
 
 
 public class BIBTEX extends Format{
 	
-	String input, prop;
-	StringReader reader;
+	private String input, prop;
+	private StringReader reader;
+	private Map<String,String> fieldMap;
+	private Map<String,String> typeMap;
+	private CSF item;
 
 	public BIBTEX(String input) {
 		super(input);
 		this.input = input;
 		reader = new StringReader(input);
 		prop = "";
+		fieldMap = new HashMap<String,String>();
+		typeMap = new HashMap<String,String>();
+		populate();
+		item = new CSF();
 		doImport();
+		try {
+			item.load(prop);
+		} catch (ConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(prop);
 	}
 
 	public BIBTEX(CSF item) {
@@ -35,22 +54,123 @@ public class BIBTEX extends Format{
 		return null;
 	}
 	
+	private void processField(String field, String value){
+		if (value.trim() == "")
+			return;
+		if(fieldMap.containsKey(field))
+			addProperty( fieldMap.get(field) , value );
+		else if( field.equals("journal") || field.equals("fjournal"))
+			if(prop.contains("publicationTitle"))
+				addProperty( "journalAbbreviation" , value);
+			else
+				addProperty("publicationTitle" ,  value) ;
+		else if(field.equals("author") || field.equals("editor") || field.equals("translator")){
+			String authors = "";
+			for(String str : Splitter.on(" and ").trimResults().split(value))
+				authors += str.replace(",", "\\,") + ", ";
+			authors = authors.substring(0, authors.lastIndexOf(","));
+			addProperty(field , authors);
+		}
+		else if( field.equals("institution") || field.equals("organization"))
+			addProperty("backupPublisher" ,  value);
+		else if( field.equals("number")){
+			if(prop.contains("itemType: report"))
+				addProperty("reportNumber" ,  value);
+			else if (prop.contains("itemType: book\n") || prop.contains("itemType: bookSection"))
+				addProperty("seriesNumber", value);
+			else
+				addProperty("issue", value);
+		}
+		else if( field.equals("month") ){}
+		else if( field.equals("year") ){
+			if(prop.contains("date:")){
+				if(!prop.contains(value))
+					;//prop = prop.substring(prop.indexOf("date: "),prop.indexOf('\n', prop.indexOf("date: ") ))
+			}
+			else
+				addProperty("date", value);
+		}
+		else if( field.equals("pages") ){
+			if(prop.contains("book\n") || prop.contains("thesis") || prop.contains("manuscript") )
+				addProperty("numPages", value);
+			else
+				addProperty("pages", value.replaceAll("--", "-"));
+		}
+		else if( field.equals("note") ){}
+		else if( field.equals("howpublished") ){}
+		else if( field.equals("keywords") || field.equals("keyword") )
+			addProperty("tags", value.replaceAll(",", ", "));
+		else if( field.equals("comment") || field.equals("annote") || field.equals("review") ){}
+		else if( field.equals("pdf") ){}
+		else if( field.equals("sentelink") ){}
+		else if( field.equals("file") ){}
+		else 
+			addProperty(field, value);
+		
+	}
+	
+	private String getFieldValue(char read){
+		String value = "";
+		try {
+			if(read == '{'){
+				int openBraces = 1;
+				while((byte)(read = (char) reader.read() ) != -1){
+					if(read == '{' && (value.length() == 0 || value.charAt(value.length()-1) != '\\')){
+						openBraces++;
+						value += read;
+					}
+					else if(read == '}' && (value.length() == 0 || value.charAt(value.length()-1) != '\\')){
+						openBraces--;
+						if(openBraces == 0 )
+							break;
+						else
+							value += read;
+					}
+					else
+						value += read;
+				}
+			}
+			else if( read == '"'){
+				int openBraces = 1;
+				while((byte)(read = (char) reader.read() ) != -1){
+					if(read == '{' && (value.length() == 0 || value.charAt(value.length()-1) != '\\')){
+						openBraces++;
+						value += read;
+					}
+					else if(read == '}' && (value.length() == 0 || value.charAt(value.length()-1) != '\\')){
+						openBraces--;
+						value += read;
+					}
+					else if(read == '"' && openBraces == 0)
+						break;
+					else
+						value +=read;
+				}
+			}
+		} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+		return value.replaceAll("[{}\\\\]", "");
+	}
+	
+	
 	private void beginRecord(String type, char closeChar){
 		String value = "";
 		String field = "";
 		type = CharMatcher.WHITESPACE.trimAndCollapseFrom(type.toLowerCase(), ' ');
 		if(!type.equals("string")){
-			String itemType = type;//from map
+			String itemType = typeMap.containsKey(type) ? typeMap.get(type) :  type;//from map
 			char read;
 			//if not in map, error
-			prop += "itemType: " + itemType +"\n";
+			addProperty("itemType", itemType);
 			try{
-				while((read = (char) reader.read()) > 0){
+				while((byte)(read = (char) reader.read() ) != -1){
+//					System.out.println(read);
 					if(read == '='){
 						do
 							read = (char) reader.read();
 						while(testWhiteSpace(read));
-						
 						if(testAlphaNum(read)){
 							value = "";
 							do{
@@ -60,10 +180,12 @@ public class BIBTEX extends Format{
 							
 							//check map for value
 						}
-						else;//
+						else
+							value = getFieldValue(read);//
 							//get from map [read]
 						//process item
-						System.out.println("Field: " + field + " Value: " + value);
+//						System.out.println("Field: " + field + " Value: " + value);
+						processField(field,value);
 						field = "";
 					}
 					else if( read == ',')
@@ -102,8 +224,8 @@ public class BIBTEX extends Format{
 					}
 					else if(testAlphaNum(read))
 						type += read;
-				if(!type.equals("false"))
-					System.out.println(type);
+//				if(!type.equals("false"))
+//					System.out.println(type);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -116,6 +238,39 @@ public class BIBTEX extends Format{
 	}
 	
 	private boolean testWhiteSpace(char c){
-		return c == '\n' || c == '\r' || c ==  't' || c == ' ';
+		return c == '\n' || c == '\r' || c ==  '\t' || c == ' ';
+	}
+	
+	private void populate(){
+		fieldMap.put("address", "place");
+		fieldMap.put("chapter", "section");
+		fieldMap.put("copyright", "rights");
+		fieldMap.put("isbn", "ISBN");
+		fieldMap.put("issn", "ISSN");
+		fieldMap.put("iccn", "callNumber");
+		fieldMap.put("location", "archiveLocation");
+		fieldMap.put("shorttitle", "shortTitle");
+		fieldMap.put("doi", "DOI");
+		fieldMap.put("booktitle", "publicationTitle");
+		fieldMap.put("school", "publisher");
+		fieldMap.put("institution", "publisher");
+		
+		typeMap.put("article","journalArticle");
+		typeMap.put("inbook","bookSection");
+		typeMap.put("incollection","bookSection");
+		typeMap.put("phdthesis","thesis");
+		typeMap.put("unpublished","manuscript");
+		typeMap.put("inproceedings","conferencePaper");
+		typeMap.put("conference","conferencePaper");
+		typeMap.put("techreport","report");
+		typeMap.put("booklet","book");
+		typeMap.put("manual","book");
+		typeMap.put("mastersthesis","thesis");
+		typeMap.put("misc","book");
+		typeMap.put("proceedings","book");
+	}
+	
+	private void addProperty(String field, String value){
+		prop += field + ": " + value + "\n";
 	}
 }
