e36c1e211a134e7fea54d6929c386abe0444f344
hab278 <as>
2012-09-19 12:35:39 -0400

    Formatted Formats

--- src/main/java/edu/nyu/library/citation/Formats.java
+++ src/main/java/edu/nyu/library/citation/Formats.java
@@ -1,8 +1,10 @@
 package edu.nyu.library.citation;
+
 /**
  * enum type for the various supported formats
+ * 
  * @author hab278
- *
+ * 
  */
 public enum Formats {
 	/**
@@ -10,24 +12,30 @@ public enum Formats {
 	 */
 	PNX,
 	/**
-	 * XERXES XML. A format used be XERXES. Can be converted to RIS {@link Formats#RIS}, so functionality not currently required.
+	 * XERXES XML. A format used be XERXES. Can be converted to RIS
+	 * {@link Formats#RIS}, so functionality not currently required.
 	 */
 	XERXES_XML,
 	/**
-	 * OpenURL. A standard method of storing key-value pairs within a URL. Used by Umlaut, which is an OpenURL resolver.
+	 * OpenURL. A standard method of storing key-value pairs within a URL. Used
+	 * by Umlaut, which is an OpenURL resolver.
 	 */
 	OPENURL,
 	/**
-	 * Research Information Systems. A file format developed by Research Information Systems that is used by many citation manager tools, including RefWorks.
+	 * Research Information Systems. A file format developed by Research
+	 * Information Systems that is used by many citation manager tools,
+	 * including RefWorks.
 	 */
 	RIS,
 	/**
-	 * BibTeX Citation Management (LaTeX). This is used with LaTeX documents to cite sources.
+	 * BibTeX Citation Management (LaTeX). This is used with LaTeX documents to
+	 * cite sources.
 	 */
 	BIBTEX,
 	/**
-	 * Citation Standard Format. A format designed specifically for this application. 
+	 * Citation Standard Format. A format designed specifically for this
+	 * application.
 	 */
 	CSF;
-	
+
 }
