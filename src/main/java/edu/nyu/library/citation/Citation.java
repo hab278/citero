68f748a7559efe32288f00e30c3549cb1f7ab14b
hab278 <as>
2012-09-24 16:10:34 -0400

    Docs added

--- src/main/java/edu/nyu/library/citation/Citation.java
+++ src/main/java/edu/nyu/library/citation/Citation.java
@@ -23,25 +23,62 @@ public class Citation {
 	/** item variable is a CSF object that contains the data. */
 	private static CSF item;
 
-	
-	private Citation( String data ){
+	/**
+	 * Creates a Citation instance and loads the provided data
+	 * 
+	 * @param data
+	 *            Input data represented as a string
+	 */
+	private Citation(String data) {
 		Citation.data = data;
 	}
-	
-	public static Citation map(String data){
+
+	/**
+	 * Returns a citation object with the loaded data.
+	 * 
+	 * @param data
+	 *            Input data represented as a string
+	 * @return A Citation object with the loaded data.
+	 */
+	public static Citation map(String data) {
 		return new Citation(data);
 	}
-	public Citation from(Formats format){
+
+	/**
+	 * Returns a citation object with the loaded data and format.
+	 * 
+	 * @param format
+	 *            The format the input data came in.
+	 * @return A Citation object with the loaded data and format.
+	 */
+	public Citation from(Formats format) {
 		return new Citation(Citation.data, format);
 	}
-	public static Citation from(CSF file){
+
+	/**
+	 * Returns a citation object with the data loaded from a CSF object.
+	 * 
+	 * @param file
+	 *            A CSF object containing bibliographic data.
+	 * @return A Citation object with data loaded from a CSF object.
+	 */
+	public static Citation from(CSF file) {
 		return new Citation(file);
 	}
-	public String to(Formats output){
+
+	/**
+	 * Converts data to the specified output format in string representation.
+	 * 
+	 * @param output
+	 *            The format the data should be converted to
+	 * @return A string representation of the converted data.
+	 */
+	public String to(Formats output) {
 		return output(output);
 	}
+
 	/**
-	 * Creates a Citation instance and loads the provided data.
+	 * Creates a Citation instance and loads the provided data and format.
 	 * 
 	 * @param data
 	 *            Input data represented as a string
@@ -50,7 +87,8 @@ public class Citation {
 	 * @throws IllegalArgumentException
 	 *             derived from {@link Citation#loadData(String, Formats)}
 	 */
-	private Citation(String data, Formats input) throws IllegalArgumentException {
+	private Citation(String data, Formats input)
+			throws IllegalArgumentException {
 		logger.info("MAIN CITATION TOOL");
 		format = input;
 		Citation.data = data;
