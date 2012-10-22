6d03554d7aaad01be457ecebc5f5e4f407fa4cb3
hab278 <as>
2012-10-22 16:23:49 -0400

    fixed Namer Test

--- src/test/java/edu/nyu/library/citation/NamerTest.java
+++ src/test/java/edu/nyu/library/citation/NamerTest.java
@@ -1,7 +1,6 @@
 package edu.nyu.library.citation;
 
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class NamerTest {
@@ -9,6 +8,6 @@ public class NamerTest {
 	@Test
 	public void FirstNameTest(){
 		Namer n = new Namer("Eddie Murphy 1961-");
-		assertTrue(n.goodName().equals("Muprhy, Eddie"));
+		assertEquals(("Murphy, Eddie"), n.goodName());
 	}
 }
