fddbe5c93d440e3b076c6fd228c6b9252c71460e
hab278 <as>
2013-04-22 18:48:48 -0400

    Some headway in getting citeproc, using rhino

---
+++ src/test/java/edu/nyu/library/citero/CiteProcTest.java
@@ -0,0 +1,45 @@
+package edu.nyu.library.citero;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import org.mozilla.javascript.*;
+import org.mozilla.javascript.tools.shell.Global;
+
+public class CiteProcTest {
+
+    @Test
+    public void citeProc() {
+        ContextFactory factory = new ContextFactory();
+        Context cx = factory.enterContext();
+
+        Global global = new Global(cx);
+
+        try {
+            FileReader xmle4x = new FileReader("/Users/hannan/Documents/workspace/citero/src/main/java/edu/nyu/library/citero/xmle4x.js");
+            FileReader citeproc = new FileReader("/Users/hannan/Documents/workspace/citero/src/main/java/edu/nyu/library/citero/citeproc.js");
+            FileReader chicago = new FileReader("/Users/hannan/Documents/workspace/citero/src/main/java/edu/nyu/library/citero/chicago.js");
+            Object result = cx.evaluateReader(global, xmle4x, "xmle4x.js", 0, null);
+            Object result2 = cx.evaluateReader(global, citeproc, "citeproc.js", 0, null);
+            Object result3 = cx.evaluateReader(global, chicago, "chicago.js", 0, null);
+            String s = "print( get_formatted_citation() );";
+            Object res = cx.evaluateString(global, s, "<cmd>", 0, null);
+
+//            // Convert the result to a string and print it.
+//            System.out.println(result.toString());
+//            System.out.println(result2.toString());
+//            System.out.println(result3.toString());
+        
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
