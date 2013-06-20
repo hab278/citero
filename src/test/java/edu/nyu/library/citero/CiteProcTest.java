3c73338ac2a03a56c65ad515b8d1520353d9843e
hab278 <as>
2013-06-20 12:49:01 -0400

    Reordered where the JS and CSL files belong, redid how they were loaded, and fin, updated ver

--- src/test/java/edu/nyu/library/citero/CiteProcTest.java
+++ src/test/java/edu/nyu/library/citero/CiteProcTest.java
@@ -1,13 +1,20 @@
 package edu.nyu.library.citero;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class CiteProcTest {
 
     @Test
-    public void citeProc() {
-            System.out.println(Citero.map(FormatsTest.PNX).from(Formats.PNX).to(CitationStyles.MLA));
-            System.out.println(Citero.map(FormatsTest.BIBTEX).from(Formats.BIBTEX).to(CitationStyles.APA));
-            System.out.println(Citero.map(FormatsTest.XERXES_XML).from(Formats.XERXES_XML).to(CitationStyles.CHICAGO_AUTHOR_DATE));
+    public void citeProc() throws IOException {
+//            System.out.println(Citero.map(FormatsTest.PNX).from(Formats.PNX).to(CitationStyles.MLA));
+//            System.out.println(Citero.map(FormatsTest.BIBTEX).from(Formats.BIBTEX).to(CitationStyles.APA));
+//            System.out.println(Citero.map(FormatsTest.XERXES_XML).from(Formats.XERXES_XML).to(CitationStyles.CHICAGO_AUTHOR_DATE));
+        String fileName = "apa.csl";
+        
     }
 
 }
